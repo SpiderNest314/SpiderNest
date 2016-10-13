54c45cf620000fcee69be27c1e3f1d42abcc3abe
SpiderNest314 <spidernestdevelopers@gmail.com>
2016-10-13 07:24:26 -0400

    About
    This is the Section that Has Our Information compiled into one page I feel it may be a little useless since we Have a Portfolio section but this page has Better Graphics Parallax Scrolling Effect thanks to W3Schools. Ultimately Will include who we are, what we do, our overall vision in the web development Industry, our skill sets and contact info!!! Back-end not yet started!

---
+++ about.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml"    
+	xmlns:fb="http://ogp.me/ns/fb#">
+<head>
+<meta name="description" content="SpiderNest Developers Great Responsive Websites at an Affordable price. SpiderNest Offers a Wide variety of Websites Starting at $300. When A Digital Presence is so Important Dont waste time Get Your Affordable Website Now.">
+<!--ink to my style sheets for this page-->
+<!--<link rel="stylesheet" type="text/css" href="http://www.spidernest.org/css/index.css">
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>SpiderNest Developers</title>
+  <meta charset="utf-8">
+  <meta name="og:image" content="http://www.spidernest.org/img/spidereyes.jpg"/>
+  <meta property="og:title" content="SpiderNest"/>
+  <meta property="og:url" content="http://www.SpiderNest.org/about.php"/>
+  <meta property="og:site_name" content="SpiderNest"/>
+  <meta property="og:type" content="about"/>
+  
+  <meta property="og:description" content="Web Development Building Responsive Websites at an Affordable price. SpiderNest Offers a Wide variety of Websites Starting at $300. When A Digital Presence is so Important Dont waste time Get Your Affordable Website Now."/>
+	
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>/*http://192.168.1.142/Images/Royalty_Free/goals/love.jpg*/
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}body, html {height: 100%;color: #777;line-height: 1.8;}
+
+/* Create a Parallax Effect */
+.bgimg-1, .bgimg-2, .bgimg-3 {opacity: 0.7;background-attachment: fixed;background-position: center;
+  background-repeat: no-repeat;background-size: cover;}
+
+/* First image (Logo. Full height) */
+.bgimg-1 {background-image: url('http://www.spidernest.org/img/spidereyes.jpg');min-height: 100%;}
+
+/* Second image (Portfolio) */
+.bgimg-2 {background-image: url("http://www.spidernest.org/about/welove.jpg");min-height: 400px;}
+
+/* Third image (Contact) */
+.bgimg-3 {
+    background-image: url("http://www.spidernest.org/about/whitepc.jpg");
+    min-height: 400px;
+}
+.w3-wide {letter-spacing: 10px;}
+.w3-hover-opacity {cursor: pointer;}
+
+#googleMap {width: 100%;height: 400px;-webkit-filter: grayscale(90%);filter: grayscale(90%);}
+
+/* Turn off parallax scrolling for tablets and mobiles */
+@media only screen and (max-width: 1024px) {.bgimg-1, .bgimg-2, .bgimg-3 {background-attachment: scroll;}}
+.h1{position: absolute;top:10%;left:48%;transform: translate(-50%,-50%);font-size:30px;color:#129a92;text-shadow:2px 2px #000000;}
+</style>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <ul class="w3-navbar" id="myNavbar">
+    <li><a href="http://www.spidernest.org" class="w3-padding-large">HOME</a></li>
+    <li class="w3-hide-small w3-right">
+      <a href="#" class="w3-padding-large w3-hover-red"><i class="fa fa-search"></i></a>
+    </li>
+  </ul>
+</div>
+
+<!-- First Parallax Image with Logo Text -->
+<div class="bgimg-1 w3-opacity w3-display-container">
+  <div class="w3-display-middle" style="white-space:nowrap;">
+    <!--<span class="w3-center w3-padding-xlarge w3-black w3-xlarge w3-wide w3-animate-opacity"><span class="w3-hide-small">&lt;/SpiderNest&gt;</span></span>-->
+
+    
+
+    
+  </div>
+</div>
+
+<!-- Container (About Section) -->
+<div class="w3-content w3-container w3-padding-64" id="about">
+  <h3 class="w3-center">ABOUT US</h3>
+  <p class="w3-center"><em>Our Vision!</em></p>
+  <p>Developers for the Web. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+    qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <div class="w3-row">
+    <div class="w3-col m6 w3-center w3-section">
+      <p><b>Who We Are!</b></p><br>
+      <img src="http://www.spidernest.org/img/darknet2.jpg" class="w3-circle" height="200" width="250" alt="Spider Nest">
+    </div>
+
+    <!-- Hide this text on small devices -->
+    <div class="w3-col m6 w3-hide-small w3-section">
+      <p>Welcome to SpiderNest Website. We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
+
+<!-- Second Parallax Image with Portfolio Text -->
+<div class="bgimg-2 w3-display-container">
+  <div class="w3-display-middle">
+    <span class="w3-xxlarge w3-text-light-grey w3-wide">PROGRAMMING!</span>
+  </div>
+</div>
+
+<!-- Container (Portfolio Section) -->
+<div class="w3-content w3-container w3-padding-64">
+  <h3 class="w3-center">OUR SKILLS</h3>
+  <p class="w3-center"><em>Front-End & Back-End Designing and Functionality.<br>Wide Set of Tools and Skills to Get the Job Done! </em></p><br>
+
+  <!-- Responsive Grid. Four columns on tablets, laptops and desktops. Will stack on mobile devices/small screens (100% width) -->
+  <div class="w3-row-padding w3-center">
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+  </div>
+
+  <div class="w3-row-padding w3-center w3-section">
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="http://www.spidernest.org/img/darknet2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity">
+    </div>
+  </div>
+</div>
+
+<!-- Modal for full size images on click-->
+<div id="modal01" class="w3-modal w3-black" onclick="this.style.display='none'">
+  <span class="w3-closebtn w3-hover-red w3-text-white w3-xxxlarge w3-container w3-display-topright">×</span>
+  <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+    <img id="img01" style="max-width:100%">
+  </div>
+</div>
+
+<!-- Third Parallax Image with Portfolio Text -->
+<div class="bgimg-3 w3-display-container">
+  <div class="w3-display-middle">
+     <span class="w3-xxlarge w3-text-light-grey w3-wide">CONTACT US!</span>
+  </div>
+</div>
+
+  
+<!-- Container (Contact Section) -->
+<div id="contact" class="container-fluid bg-grey">
+  <h2 class="text-center" ><font color="#129a92">Contact Us</font></h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p ><font size="4"><strong>SpiderNest Developers</strong> Work hard to Build Your Website and  stay up to date on Technology whether it is Design,Site Functionality or Security We want to Deliver the Best Possible Product to our Customers.<br /> We offer a Set Variety of Expertise <ul><li>Web Design and Graphics</li> 
+      <li>Responsive Interactive Websites</li> <li>Back end Functionality</li> <li>Website Security</li></ul>
+       We Hope You Choose Us for your Digital Marketing Strategy.</font></p>
+      <!--<p><span class="glyphicon glyphicon-map-marker"></span> , </p>-->
+      <!--<p><span class="glyphicon glyphicon-phone"></span> +(555) 555-5555</p>-->
+      <p>       <span class="glyphicon glyphicon-envelope"></span><strong> SpiderNest@gmail.com</strong></p>
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+         <input class="form-control" id="content" name="content" placeholder="What Type of Website?" type="content" required>
+      </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+        <div class="col-sm-6 form-group" >
+          
+          <select class ="form-control" name="Selection" >
+            <option value="Automotive">Select Type of Business</option>
+            <option value="Automotive">Automotive</option>
+            <option value="Beauty">Beauty</option>
+            <option value="Customer service">Customer Service</option>
+            <option value="Estate">Real-Estate</option>
+            <option value="Financial Adviser">Financial Advisor</option>
+            <option value="Food">Food</option>
+            <option value="Home Services">Home Services</option>
+            <option value="Insurance">Insurance</option>
+            <option value="labor">Labor</option>
+            <option value="Legal">Legal</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Mortgage">Mortgage</option>
+            <option value="Promotional">Promotional</option>
+            <option value="sales">Sales</option>
+            <option value="Technology">Technology</option>
+            <option value="Wellness">Wellness</option>
+            <option value="Wellness">Other</option>
+         </select><br>
+      </div>
+      
+      <div class="col-sm-6 form-group">
+         <input class="form-control" id="country" name="country" placeholder="What Country are you from?" type="country" required>
+      </div>
+      <div class="col-sm-6 form-group">
+         <input class="form-control" id="state" name="State" placeholder="Whats Your State/Province" type="state" required>
+      </div>
+     
+      </div>
+      <textarea class="form-control" id="comment" name="comments" placeholder="Questions or Comments About Your Future Website?" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button style="background-color:#129a92; color:#ffffff" type="button" class="btn">Submit Info</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-dark-grey w3-padding-48 w3-hover-black">
+  <p> <a href="#" title="SpiderNest" target="_blank" class="w3-hover-opacity">&lt;/SpiderNest&gt;</a></p>
+</footer>
+ 
+<script type="text/javascript>
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-navbar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+    }
+}
+</script>
+
+</body>
+</html>
+
